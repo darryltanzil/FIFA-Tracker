0f10948638d8d593d3bb623efb8644358834da3f
Darryl Tanzil <darryltanzil@gmail.com>
2023-02-25 00:16:35 -0800

    update: adding get request to ComparisonController

--- src/main/java/com/darryltanzil/FIFASTAT/ComparisonController.java
+++ src/main/java/com/darryltanzil/FIFASTAT/ComparisonController.java
@@ -2,18 +2,54 @@ package com.darryltanzil.FIFASTAT;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.swing.*;
 
 @RestController
 public class ComparisonController {
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/comparison")
     public Comparison comparison(@RequestParam(value = "player1", defaultValue = "None") String player1,
                                  @RequestParam(value = "player2", defaultValue = "None") String player2) {
-        return new Comparison(counter.incrementAndGet(), String.format(template, player1), "No winner yet");
+        Player player1Obj = getPlayerData(player1);
+        return new Comparison(counter.incrementAndGet(), "test", "No winner yet");
     }
+
+    /**
+     *  GETs player in the form of the Player object
+     * @param player the id of player
+     * @return the player Object
+     */
+    public Player getPlayerData(String player) {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiKey = System.getenv("APIKEY");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-AUTH-TOKEN", "apiKey");
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        ResponseEntity<PlayerJSON> player1Response = restTemplate.exchange(
+                "https://futdb.app/api/players/" + player,
+                HttpMethod.GET,
+                entity,
+                PlayerJSON.class
+        );
+
+        try {
+            return player1Response.getBody().player();
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Error: No player with id: " + player, e);
+        }
+    }
+
+
 }
